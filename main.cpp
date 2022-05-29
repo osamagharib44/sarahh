120ca47f7b30c2d5d0dc9becb3224a2481b0996d
osamagharib44 <103857636+osamagharib44@users.noreply.github.com>
2022-05-30 01:52:29 +0200

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,11 +1,15 @@
 #include<bits/stdc++.h>
+#include<fstream>
 #include "User.h"
 #include "Message.h"
 using namespace std;
 
 int curid = 0;
 int amount = 0;
+string name,pass;
 User users[100];
+map<string, int> MapNameToId;
+bool EndProgram = false;
 
 void print(const string& x){
     cout << x << endl;
@@ -16,6 +20,7 @@ int getinput(int mx){
     while (true){
         cout <<"Enter input:";
         cin >> in;
+        cin.ignore();
         if (!mx){
             return in;
         }
@@ -32,66 +37,65 @@ void printseparator(){
     print("----------------------------");
 }
 
-int main(){
+int CheckName(string str){
+    transform(str.begin(), str.end(), str.begin(),::tolower);
+    return MapNameToId[str];
+}
 
-    //for debug
-    users[1] = User("osama","ok",1);
-    amount++;
-    users[2] = User("mr underwear","ok",2);
-    amount++;
-    users[3] = User("3shmawy elguard elshrs","ok",3);
-    users[3].sendMessage(1,"please work", users);
-    amount++;
-    //
+bool Validator(string str){
+    for (auto c : str){
+        if (c<33 || c>126 || isspace(c)) {
+            return false;
+        }
+    }
+    return true;
+}
 
-    print("Welcome to curious!"); printseparator();
-    print("1 - Register");
-    print("2 - Login"); printseparator();
-    string name;
-    string pass;
-
-    /*----------------------------*/
+void DoLoginRegister(){
 
     switch (getinput(2)) {
         case 1:
-            printseparator();
             while (true){
-                cout <<"Enter your username:";
-                cin >> name;
-                bool taken = false;
-                for (int i = 1; i<=amount; i++){
-                    if (users[i].GetUsername() == name){
-                        taken = true;
-                    }
+                printseparator();
+                print("Enter your username:");
+                getline(cin,name);
+                bool valid = Validator(name);
+                if (!valid){
+                    print("Username must contain only letters, numbers and special characters.");
+                    continue;
                 }
+                transform(name.begin(), name.end(), name.begin(),::tolower);
+                bool taken = CheckName(name);
                 if (taken){
-                    print("Username is taken!!");
+                    print("Username is taken.");
+                    continue;
                 }
-                else{
-                    break;
-                }
+                break;
             }
             cout << "Enter your password:";
             cin >> pass;
-            curid = amount + 1;
+            amount++;
+            curid = amount;
             users[curid] = User(name,pass,curid);
+            MapNameToId[name] = curid;
             break;
         case 2:
             while (true){
                 printseparator();
-                cout <<"Enter your username:";
-                cin >> name;
-                bool taken = false;
-                for (int i = 1; i<=amount; i++){
-                    if (users[i].GetUsername() == name){
-                        taken = true;
-                        curid = i;
-                    }
+                print("Enter your username:");
+                getline(cin,name);
+                bool valid = Validator(name);
+                if (!valid){
+                    print("Username must contain only letters, numbers and special characters.");
+                    continue;
                 }
+                transform(name.begin(), name.end(), name.begin(),::tolower);
+                bool taken = CheckName(name);
                 if (!taken){
-                    print("Account doesn't exist");
+                    print("Account doesn't exist.");
                 }
                 else{
+                    curid = CheckName(name);
                     cout << "Enter your password:";
                     cin >> pass;
                     if (users[curid].GetPassword()==pass){
@@ -104,112 +108,146 @@ int main(){
             }
             break;
     }
+}
+
+void GetDataFromFile(){
+    ifstream data("data.txt");
+    string line;
+    string user;
+    string pass;
+    int id = 0;
+    while (getline(data,line)){
+        //read user and pass
+        id++;
+        amount++;
+        getline(data,user);
+        getline(data,pass);
+        users[id] = User(user, pass, id);
+        MapNameToId[user] = id;
+
+        //read rest of data
+        users[id].ReadDataFromFile(data);
+    }
+    data.close();
+
+}
+
+void SaveDataToFile(){
+    ofstream data;
+    data.open("data.txt", ofstream::out | ofstream::trunc);
+    for (int i = 1; i<=amount;i++){
+       users[i].SaveDataToFile(data);
+    }
+    data.close();
+}
+
+int GetTargetId(){
+    string targname;
+    int targid;
+    while (true){
+        cout << "Contact's name:";
+        cin >> targname;
+        transform(targname.begin(), targname.end(), targname.begin(),::tolower);
+        targid = MapNameToId[targname];
+        if (targid<1 || targid>amount || targid==curid){
+            print("Invalid Username");
+        }
+        else {
+            break;
+        }
+    }
+    return targid;
+}
+
+void DoOperations(){
+    printseparator();
+    int in = getinput(11);
+
+    string x;
+    int targid;
+    switch (in) {
+        case 1:
+            users[curid].addContact(GetTargetId());
+            break;
+        case 2:
+            cout << "Username to search for:";
+            cin >> x;
+            users[curid].searchForContact(x, users);
+            break;
+        case 3:
+            User::displayContacts(curid, users);
+            break;
+        case 4:
+            targid = GetTargetId();
+            cout <<"Enter message:" << endl;
+            getline(cin,x);getline(cin,x);
+            users[curid].sendMessage(targid, x, users);
+            break;
+        case 5:
+            users[curid].undoLastMessage(users);
+            print("Last sent message deleted");
+            break;
+        case 6:
+            users[curid].viewSentMessages(users);
+            break;
+        case 7:
+            targid = GetTargetId();
+            users[curid].getMessagesFrom(targid);
+            break;
+        case 8:
+            targid = GetTargetId();
+            users[curid].addRecentMsgToFav(targid);
+            break;
+        case 9:
+            users[curid].viewAllFavMsgs();
+            break;
+        case 10:
+            users[curid].removeOldestFavMsg();
+            break;
+        case 11:
+            EndProgram = true;
+            break;
+    }
+    printseparator();
+    if (!EndProgram) {
+        print("Enter any key to continue.");
+        getinput(0);
+        printseparator();
+    }
+}
+
+int main(){
+    MapNameToId["test"] = 3;
+    GetDataFromFile();
+    print("Welcome to curious!"); printseparator();
+    print("1 - Register");
+    print("2 - Login"); printseparator();
+    string name;
+    string pass;
+
+    /*----------------------------*/
+
+    DoLoginRegister();
     printseparator();
     cout << "Welcome back, "<<users[curid].GetUsername()<<"!" << endl;
     printseparator();
     /*----------------------------*/
 
-    bool endprogram = false;
-    while (!endprogram) {
+    while (!EndProgram) {
         print("1 - Add a contact");
         print("2 - Search for a contact");
         print("3 - Display all your contacts");
         print("4 - Send a message");
         print("5 - Undo last sent message");
-        print("6 - View all messages from a specific contact");
-        print("7 - Favourite last received message from specific contact");
-        print("8 - Display all favourite messages");
-        print("9 - Remove oldest favourite message");
-        print("10 - Logout");
+        print("6 - View all sent messages");
+        print("7 - View all messages from a specific contact");
+        print("8 - Favourite last received message from specific contact");
+        print("9 - Display all favourite messages");
+        print("10 - Remove oldest favourite message");
+        print("11 - Logout");
 
-        printseparator();
-        int in = getinput(10);
-
-        int targid = 0;
-        string x;
-        string msg;
-        switch (in) {
-            case 1:
-                while (true){
-                    cout << "Contact's ID:";
-                    cin >> targid;
-                    if (targid<1 || targid>amount || targid==curid){
-                        print("Invalid ID");
-                    }
-                    else {
-                        break;
-                    }
-                }
-                users[curid].addContact(targid);
-                break;
-            case 2:
-                cout << "Name:";
-                cin >> x;
-                users[curid].searchForContact(x, users);
-                break;
-            case 3:
-                User::displayContacts(curid, users);
-                break;
-            case 4:
-                while (true){
-                    cout << "Contact's ID:";
-                    cin >> targid;
-                    if (targid<1 || targid>amount || targid==curid){
-                        print("Invalid ID");
-                    }
-                    else {
-                        break;
-                    }
-                }
-                cout <<"Enter message:" << endl;
-                getline(cin,x);getline(cin,x);
-                users[curid].sendMessage(targid, x, users);
-                break;
-            case 5:
-                users[curid].undoLastMessage(users);
-                break;
-            case 6:
-                while (true){
-                    cout << "Contact's ID:";
-                    cin >> targid;
-                    if (targid<1 || targid>amount || targid==curid){
-                        print("Invalid ID");
-                    }
-                    else {
-                        break;
-                    }
-                }
-                users[curid].getMessagesFrom(targid);
-                break;
-            case 7:
-                while (true){
-                    cout << "Contact's ID:";
-                    cin >> targid;
-                    if (targid<1 || targid>amount || targid==curid){
-                        print("Invalid ID");
-                    }
-                    else {
-                        break;
-                    }
-                }
-                users[curid].addRecentMsgToFav(targid);
-                break;
-            case 8:
-                users[curid].viewAllFavMsgs();
-                break;
-            case 9:
-                users[curid].removeOldestFavMsg();
-                break;
-            case 10:
-                endprogram = true;
-                break;
-        }
-        printseparator();
-        if (!endprogram) {
-            print("Enter any key to continue.");
-            getinput(0);
-            printseparator();
-        }
+        DoOperations();
     }
     cout <<"Goodbye, "<< users[curid].GetUsername() <<"!"<<endl;
+    SaveDataToFile();
 }
